e6bfbd433d93e453c03173ed2c15d382d73c47a5
Manuel González <magandrez@gmail.com>
2016-02-08 10:57:44 +0200

    Fixes certificates path

--- server/config.py
+++ server/config.py
@@ -3,6 +3,6 @@
 
 import os
 
-PYBANKID_KEY_PATH = os.path.join(os.path.dirname(__file__), 'keys/key.pem')
-PYBANKID_CERT_PATH = os.path.join(os.path.dirname(__file__), 'keys/cert.pem')
+PYBANKID_KEY_PATH = os.path.join(os.path.dirname(__file__), '../keys/key.pem')
+PYBANKID_CERT_PATH = os.path.join(os.path.dirname(__file__), '../keys/cert.pem')
 PYBANKID_TEST_SERVER = True
