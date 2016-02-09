6304606e6b815ad202518bca72a98c98b997e067
Manuel G <manuel@manuel.is>
2016-02-09 10:15:10 +0200

    Adds Basic Auth
    Uses Flask-BasicAuth package to implement a simple auth. Credentials are stored as environment variables

--- server/__init__.py
+++ server/__init__.py
@@ -2,8 +2,17 @@
 #coding: utf-8
 
 from flask import Flask
+from flask.ext.basicauth import BasicAuth
 from flask_pybankid import PyBankID
+import os
 
 app = Flask(__name__)
+
+app.config['BASIC_AUTH_USERNAME'] = os.getenv('USR')
+app.config['BASIC_AUTH_PASSWORD'] = os.getenv('PASS')
+app.config['BASIC_AUTH_FORCE'] = True
+
+basic_auth = BasicAuth(app)
+
 app.config.from_object('server.config')
 bankid = PyBankID(app)
